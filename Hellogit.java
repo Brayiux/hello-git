57d9a928d8fe7f398174c389a9007d19984c9b73
Brayan Montiel <braymontiel77@gmail.com>
2025-11-15 12:08:42 -0600

    Ahora sí se actualiza el texto del print

--- Hellogit.java
+++ Hellogit.java
@@ -1,6 +1,5 @@
 public class Hellogit {
     public static void main(String[] args) {
-        System.out.println("Hola git");
-        System.out.println("¿Qué tal?");
+        System.out.println("New Hello Git");
     }
 }
